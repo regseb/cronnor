68edfa650d147fc553e9f88de860189b8ef01904
regseb <regseb@gmail.com>
2015-02-22 18:51:51 +0100

    Ajout de la gestion de AMD et CommonJS.

--- README.md
+++ README.md
@@ -4,22 +4,95 @@
 [Site Internet](https://regseb.github.io/scronpt)
 
 ## Description
-La bibliothèque JavaScript **Scronpt** ajoute la fonction **`setCron()`** qui
-est similaire à la fonction `setInterval()`, mais prend en paramètre une chaine
-de caractères indiquant les horaires d'exécution.
+La bibliothèque JavaScript **Scronpt** fourni une classe **`Cron`** pour créer
+des tâches récurentes.
 
-La bibliothèque fournit aussi la fonction `clearCron()` qui supprime une tâche.
+## API
+### Constructeur
+```JavaScript
+new Cron(notation, [état], fonction, [, param1, param2, ...])
+```
+où
+- `notation` contient la notation *cron* indiquant quand sera appelé la
+  `fonction` ;
+- `état` est un booléen indiquant si la tâche est active : `true` (valeur par
+  défaut), sinon : `false` ;
+- `fonction` est la fonction qui sera appelée à chaque horaire indiqué dans la
+  `notation` ;
+- `param1`, `param2`, `...` sont des paramètres qui seront passés à la
+  `fonction`.
+Le constructeur peut lancer une exception :
+- `Error` si la syntaxe de la notation *cron* est incorrecte ;
+- `RangeError` si un intervalle est invalide (hors limite ou la borne supérieure
+  est plus grand que la borne inférieure).
 
-## Installation
-Il suffit de télécharger le script et de l'inclure dans la page (avant la
-première utilisation des fonctions `setCron()` ou `clearCron()`).
+### Méthodes
+```JavaScript
+Cron.prototype.start()
+```
+Activer la tâche. Si la tâche est déjà active : la méthode n'a aucun effet.
+
+```JavaScript
+Cron.prototype.stop()
+```
+Désactiver la tâche. Si la tâche est déjà inactive : la méthode n'a aucun effet.
+
+```JavaScript
+Cron.prototype.status()
+```
+Récupérer l'état de la tâche. La méthode retourne `true` si la tâche est
+active ; et `false` pour une tâche inactive.
+
+## Téléchargement
+Vous pouvez récupérer le script minifié en vous rendant sur le site Internet de
+la bibliothèque : http://regseb.github.io/scronpt/.
+
+Si vous utiliser [npm](https://www.npmjs.org/ "Node Packaged Modules") ou
+[bower](http://bower.io/) : la bibliothèque est disponible avec les lignes de
+commande suivantes :
+```
+npm install scronpt
+bower install scronpt
+```
+
+## Utilisation
+### AMD
+Pour l'utilisation de la bibliothèque avec un chargeur
+[AMD](https://github.com/amdjs/amdjs-api "Asynchronous Module Definition") (par
+exemple [RequireJS](http://requirejs.org/)) :
+```JavaScript
+require(["scronpt"], function (Cron) {
+    // ...
+    var cron = new Cron(/* ... */);
+});
+// Ou :
+define(["scronpt"], function (Cron) {
+    // ...
+    var cron = new Cron(/* ... */);
+});
+```
+
+### CommonJS
+Si vous souhaitez utiliser la bibliothèque dans [node.js](http://nodejs.org/)
+(qui utilise le protocole CommonJS), voici un exemple :
+```JavaScript
+var Cron = require("scronpt");
+// ...
+var cron = new Cron(/* ... */);
+```
+
+### Variable global
+Il suffit de télécharger le script et de l'inclure dans la page HTML.
 ```HTML
 <!DOCTYPE html>
 <html>
   <head>
     <!-- ... -->
-    <script src="scronpt.min.js"></script>
+    <script src="scronpt.js"></script>
     <!-- ... -->
+    <script>
+      var cron = new Cron(/* ... */);
+    </script>
   </head>
   <body>
     <!-- ... -->
@@ -27,21 +100,8 @@ première utilisation des fonctions `setCron()` ou `clearCron()`).
 </html>
 ```
 
-## Syntaxe
-```JavaScript
-cronID = window.setCron(fonction, cron[, param1, param2, ...]);
-window.clearCron(cronID);
-```
-où
-- `cronID` est un identifiant unique de tâche, qui peut être passé à
-  `clearCron()` ;
-- `fonction` est la fonction qui doit être appelée à l'horaire ;
-- `cron` est les horaires quand seront exécuté la `fonction` ;
-- `param1`, `param2`, `...` sont des paramètres qui seront passés à la
-  `fonction`.
-
 ## Notations
-Le paramètre `cron` est une chaine de caractères composées de cinq éléments
+Le paramètre `notation` est une chaine de caractères composées de cinq éléments
 séparés par une espace. Les éléments représentent :
 
 1. les minutes : `0` à `59` ;
@@ -56,30 +116,30 @@ Pour chaque élément, des compositions sont possibles :
 - `/` : indiquer le pas (`2-6/2` corresponds aux unités `2`, `4` et `6`) ;
 - `,` : créer une liste (`4,8` corresponds aux unités `4` et `8`).
 
-Il existe aussi des raccourcis :
+Il existe aussi des chaines spéciales :
 - `"@yearly"` ou `"@annually"` : tous les ans, le 1er janvier ;
 - `"@monthly"` : le 1er jour de chaque mois ;
 - `"@weekly"` : une fois par semaine, le dimanche ;
 - `"@daily"` ou `"@midnight"` : tous les jours à minuit ;
 - `"@hourly"` : toutes les heures.
 
+Pour plus d'information, vous pouvez consulter le [manuel de `crontab`]
+(http://manpages.debian.org/cgi-bin/man.cgi?query=crontab&sektion=5&locale=fr).
+
 ## Exemples
 ```JavaScript
 // Appeler la fonction poissonDAvril tous les 1er avril à 8h00.
-var cronID = setCron(poissonDAvril, "0 8 1 apr *");
-
-// Appeler la fonction jouerAuLoto, avec les numéros à jouer en
-// paramètres, tous les vendredis 13 à 13h13.
-setCron(jouerAuLoto, "13 13 13 * fri", 19, 42, 30, 48, 44, 10);
+var cron = new Cron("0 8 1 apr *", poissonDAvril);
 
 // Appeler la fonction anonyme toutes les cinq minutes au travail (entre
 // 9h et 14h puis entre 14h et 18h) en semaine (du lundi au vendredi).
-setCron(function() {
-    alert("Tic-tac !");
-}, "*/5 9-12,14-18 * * 1-5");
+new Cron("0,30 * * * *", alert, "Ding ! Dong !");
 
-// Supprimer la tâche du poisson d'avril.
-clearCron(cronID);
+// Arrêter la tâche du poisson d'avril, ce n'est plus de notre age.
+cron.stop();
+
+// Finalement, il n'y a pas d'age pour s'amuser.
+cron.start();
 ```
 
 ## Compatibilité
--- scronpt.js
+++ scronpt.js
@@ -1,91 +1,81 @@
-/* global window, setInterval, clearInterval, setTimeout */
+/* @flow */
+/* global setInterval, clearInterval, setTimeout, define, module */
 
 /**
- * @file Bibliothèque JavaScript Scronpt implémentant un programme Unix cron.
+ * Bibliothèque JavaScript Scronpt implémentant un programme Unix cron.
+ *
+ * @module scronpt
  * @author Sébastien Règne
- * @version 1.0.1
+ * @version 0.2
  * @license Licence Public Rien À Branler.
  */
 
-/**
- * <p>
- *   Une notation <em>cron</em> doit respecter le format suivant :
- * </p>
- * <ul>
- *   <li>
- *     <em>cron</em>
- *     <ul>
- *       <li>"@yearly"</li>
- *       <li>"@annually"</li>
- *       <li>"@monthly"</li>
- *       <li>"@weekly"</li>
- *       <li>"@daily"</li>
- *       <li>"@midnight"</li>
- *       <li>"@hourly"</li>
- *       <li><em>list</em> " " <em>list</em> " " <em>list</em> " "
- *           <em>list</em> " " <em>list</em></li>
- *     </ul>
- *   </li>
- *   <li>
- *     <em>list</em>
- *     <ul>
- *       <li><em>range</em></li>
- *       <li><em>range</em> "," <em>list</em></li>
- *     </ul>
- *   </li>
- *   <li>
- *     <em>range</em>
- *     <ul>
- *       <li>"*"</li>
- *       <li>"*" "/" <em>number</em></li>
- *       <li><em>number</em></li>
- *       <li><em>number</em> "-" <em>number</em></li>
- *       <li><em>number</em> "-" <em>number</em> "/" <em>number</em></li>
- *     </ul>
- *   </li>
- *   <li>
- *     <em>number</em>
- *     <ul>
- *       <li><em>digit</em></li>
- *       <li><em>digit</em> <em>number</em></li>
- *     </ul>
- *   </li>
- *   <li>
- *     <em>digit</em>
- *     <ul>
- *       <li>"0"</li>
- *       <li>"1"</li>
- *       <li>"2"</li>
- *       <li>"3"</li>
- *       <li>"4"</li>
- *       <li>"5"</li>
- *       <li>"6"</li>
- *       <li>"7"</li>
- *       <li>"8"</li>
- *       <li>"9"</li>
- *     </ul>
- *   </li>
- * </ul>
- */
+(function (root, factory) {
+    "use strict";
 
-(function() {
+    // Si la bibliothèque est chargée avec un AMD loader.
+    if ("function" === typeof define && define.amd)
+        define([], factory);
+    // Si c'est un CommonJS-like qui charge la bibliothèque.
+    else if ("object" === typeof exports)
+        module.exports = factory();
+    else // Sinon : un chargement classique est utilisé.
+        root.Cron = factory();
+}(this, function () {
     "use strict";
 
     /**
-     * La séquence utilisée pour déterminer l'identifiant d'une nouvelle tâche.
+     * Les formes littérales des mois et des jours de la semaine. Les autres
+     * champs (minute, heure, jour du mois) n'en ont pas. Ce sont les index des
+     * éléments dans le tableau qui sont utilisés pour la correspondance. Pour
+     * les mois : un élément est inséré au début du tableau pour que l'index du
+     * mois de janvier soit 1.
      *
-     * @type {!number}
+     * @type {!Object}
+     * @const
      * @private
      */
-    var sequence = 0;
+    var NAMES = [
+        [], [], [], // Minute, heure et jour du mois.
+        ["", "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug",
+         "sep", "oct", "nov", "dec"], // Mois.
+        ["sun", "mon", "tue", "wed", "thu", "fri", "sat"] // Jour de la semaine.
+    ];
 
     /**
-     * La liste des tâches.
+     * Les valeurs minimales et maximales pouvant être saisies dans les cinq
+     * champs (pour des valeurs simples ou des intervalles).
+     *
+     * @type {!Object}
+     * @const
+     * @private
+     */
+    var LIMITS = [
+        { "min": 0, "max": 59 }, // Minute.
+        { "min": 0, "max": 23 }, // Heure.
+        { "min": 1, "max": 31 }, // Jour du mois.
+        { "min": 1, "max": 12 }, // Mois.
+        { "min": 0, "max":  7 }  // Jour de la semaine (0 et 7 représentent le
+    ];                           // dimanche).
+
+    /**
+     * Le message d'erreur (suivi de l'expression cron) renvoyé dans
+     * l'exception.
+     *
+     * @type {!string}
+     * @const
+     * @private
+     */
+    var ERROR = "Syntax error, unrecognized expression: ";
+
+
+    /**
+     * La liste des tâches actives.
      *
      * @type {!Object}
      * @private
      */
-    var jobs = { };
+    var jobs = [];
 
     /**
      * L'identifiant de l'intervalle qui vérifie toutes les minutes si des
@@ -98,15 +88,16 @@
 
 
     /**
-     * Parser une notation cron.
+     * Analyser une notation cron.
      *
      * @param {!string} input - la notation cron.
      * @return {!Object} Retourne les conditions pour exécuter la tâche.
      * @throws {Error} Si la notation cron n'est pas valide.
+     * @throws {RangeError} Si une valeur de la notation est invalide.
      * @private
      */
-    var parse = function(input) {
-        // Remplacer les raccourcis.
+    var parse = function (input) {
+        // Remplacer les chaines spéciales.
         switch(input) {
             case "@yearly": case "@annually": input = "0 0 1 1 *"; break;
             case "@monthly":                  input = "0 0 1 * *"; break;
@@ -115,63 +106,64 @@
             case "@hourly":                   input = "0 * * * *";
         }
 
-        var fields = input.split(" ");
+        // Séparer les cinq champs (minute, heure, jour du mois, mois et jour de
+        // la semaine).
+        var fields = input.split(/\s+/);
         if (5 !== fields.length)
-            throw new Error("Syntax error, 5 fields (separated by one space)" +
-                            " expected: \"" + input + "\"");
+            throw new Error(ERROR + input);
 
-        // Remplacer les formes littérales des mois.
-        fields[3] = fields[3].replace(/jan/g,  "1")
-                             .replace(/feb/g,  "2")
-                             .replace(/mar/g,  "3")
-                             .replace(/apr/g,  "4")
-                             .replace(/may/g,  "5")
-                             .replace(/jun/g,  "6")
-                             .replace(/jul/g,  "7")
-                             .replace(/aug/g,  "8")
-                             .replace(/sep/g,  "9")
-                             .replace(/oct/g, "10")
-                             .replace(/nov/g, "11")
-                             .replace(/dec/g, "12");
-        // Remplacer les formes littérales des jours de la semaine.
-        fields[4] = fields[4].replace(/sun/g, "0")
-                             .replace(/mon/g, "1")
-                             .replace(/tue/g, "2")
-                             .replace(/wed/g, "3")
-                             .replace(/thu/g, "4")
-                             .replace(/fri/g, "5")
-                             .replace(/sat/g, "6");
+        // Parcourir les cinq champs.
+        return fields.map(function (field, i) {
+            // Gérer les notations avec un astérisque ("*" ou "*/x").
+            var result = /^\*(?:\/(\d+))?$/.exec(field);
+            if (null !== result) {
+                if (undefined === result[1]) // "*".
+                    return null;
 
-        var conds = [];
-        for (var i in fields) {
-            var cond = [];
-            var ranges = fields[i].split(",");
-            for (var j in ranges) {
-                var result = /^(\*|\d+)(-\d+)?(\/\d+)?$/.exec(ranges[j]);
-                if (null === result)
-                    throw new Error("Syntax error, unrecognized expression:" +
-                                    " \"" + input + "\"");
+                var step = parseInt(result[1], 10);
+                if (0 === step)
+                    throw new RangeError(ERROR + input);
 
-                var min  = 0,
-                    max  = Infinity,
-                    step = 1;
-
-                if ("*" !== result[1])
-                    min = max = parseInt(result[1], 10);
-                if (undefined !== result[2])
-                    max = parseInt(result[2].substr(1), 10);
-                if (undefined !== result[3])
-                    step = parseInt(result[3].substr(1), 10);
-
-                cond.push({
-                    "min":  min,
-                    "max":  max,
+                return [{
+                    "min":  0,
+                    "max":  Infinity,
                     "step": step
-                });
+                }];
             }
-            conds.push(cond);
-        }
-        return conds;
+
+            // Gérer le nom des mois ("jan", "feb", ...) et des jours de la
+            // semaine ("sun", "mon", ...).
+            var value = NAMES[i].indexOf(field.toLowerCase());
+            if (-1 !== value)
+                return [{
+                    "min":  value,
+                    "max":  value,
+                    "step": 1
+                }];
+
+            // Gérer les listes.
+            return field.split(",").map(function (range) {
+                var result = /^(\d+)(?:-(\d+)(?:\/(\d+))?)?$/.exec(range);
+                if (null === result)
+                    throw new Error(ERROR + input);
+
+                var min  = parseInt(result[1], 10);
+                var max  = parseInt(result[2], 10);
+                var step = parseInt(result[3], 10);
+
+                // Vérifier que les valeurs sont dans les intervalles.
+                if (min < LIMITS[i].min || LIMITS[i].max < min ||
+                        max < LIMITS[i].min || LIMITS[i].max < max ||
+                        max < min || 0 === step)
+                    throw new RangeError(ERROR + input);
+
+                return {
+                    "min":  min,
+                    "max":  isNaN(max) ? min : max,
+                    "step": isNaN(step) ? 1 : step
+                };
+            });
+        });
     }; // parse()
 
     /**
@@ -180,11 +172,11 @@
      * @param {!Object} conds - les conditions pour exécuter la tâche.
      * @param {!number} value - la valeur qui sera testée.
      * @return {!boolean} Retourne <code>true</code> si la tâche doit-être
-     *                    exécutée : sinon <code>false</code>.
+     *                    exécutée ; sinon <code>false</code>.
      * @private
      */
-    var valide = function(conds, value) {
-        return conds.some(function(cond) {
+    var valide = function (conds, value) {
+        return null === conds || conds.some(function (cond) {
             return cond.min <= value && value <= cond.max &&
                    0 === (value - cond.min) % cond.step;
         });
@@ -197,74 +189,150 @@
      *                        paramètres à lui passer.
      * @private
      */
-    var run = function(job) {
-        job.func.apply(window, job.args);
+    var run = function (job) {
+        job.func.apply(undefined, job.args);
     }; // run()
 
+    /**
+     * Exécuter les tâches.
+     */
+    var daemon = function () {
+        var now = new Date();
+        jobs.forEach(function (job) {
+            // Vérifier que la minute, l'heure et le mois respectent les
+            // conditions.
+            if (!valide(job.conds[0], now.getMinutes()) ||
+                    !valide(job.conds[1], now.getHours()) ||
+                    !valide(job.conds[3], now.getMonth() + 1))
+                return;
+
+            // Quand le jour du mois et le jour de la semaine sont renseignés
+            // (différent de l'astérisque), la tâche est exécutée si au moins
+            // une des deux conditions est respectée.
+            if (null !== job.conds[2] && null !== job.conds[4]) {
+                if (!valide(job.conds[2], now.getDate()) &&
+                        !(valide(job.conds[4], now.getDay()) ||
+                                0 === now.getDay() &&     // Gérer la valeur 7
+                                valide(job.conds[4], 7))) // pour le dimanche.
+                    return;
+            // Sinon : soit le jour du mois, soit le jour de la semaine ou aucun
+            // des deux ne sont renseignés. Dans ce cas, vérifier classiquement
+            // les deux conditions.
+            } else if (!valide(job.conds[2], now.getDate()) ||
+                    !(valide(job.conds[4], now.getDay()) ||
+                            0 === now.getDay() &&     // Gérer la valeur 7 pour
+                            valide(job.conds[4], 7))) // le dimanche.
+                return;
+
+            // Si toutes les conditions sont respectées : lancer la tâche en
+            // parallèle.
+            setTimeout(run, 0, job);
+        });
+    }; // daemon()
+
 
     /**
-     * Ajouter une tâche.
+     * Créer une tâche.
      *
      * @example
-     * setCron(alert, "0,30 * * * *", "Ding ! Dong !");
+     * var cron = new Cron("0,30 * * * *", alert, "Ding ! Dong !");
      *
-     * @param {!function()} func - la fonction effectuant la tâche.
-     * @param {!string}     cron - les horaires quand la tâche sera effectuée.
-     * @param {...*}        args - les paramètres qui seront passés à la
-                                   fonction.
-     * @return {!number} Retourne un identifiant pour pouvoir supprimer la tâche
-     *                   créée.
-     * @see clearCron
+     * @param {!string}     cron   - les horaires quand la tâche sera exécutée.
+     * @param {!boolean=}   status - l'état de la tâche : <code>true</code> pour
+     *                               active ; sinon <code>false</code>.
+     * @param {!function()} func   - la fonction appelée quand la tâche est
+     *                               exécutée.
+     * @param {...*}        args   - les paramètres qui seront passés à la
+                                     fonction.
+     * @return {!Object} Retourne la tâche créée.
+     * @throws {Error} Si la notation cron n'est pas valide.
      * @public
+     * @constructor
      */
-    window.setCron = function(func, cron, args) {
-        var cronID = ++sequence;
-        jobs[cronID.toString()] = {
+    var Cron = function (cron, status, func, args) {
+        // Si le paramètre "status" n'est pas fourni : réorganiser les
+        // paramètres.
+        if ("function" === typeof status) {
+            args   = Array.prototype.slice.call(arguments, 2);
+            func   = status;
+            status = true;
+        } else
+            args = Array.prototype.slice.call(arguments, 3);
+
+        this.job = {
             "conds": parse(cron),
             "func":  func,
-            "args":  Array.prototype.slice.call(arguments, 2)
+            "args":  args
         };
 
-        if (null === intervalID)
-            // Créer la boucle qui vérifiera chaque minute : si une tâche
-            // doit-être exécutée.
-            intervalID = setInterval(function() {
-                var now = new Date();
-                for (var id in jobs) {
-                    var job = jobs[id];
-                    if (valide(job.conds[0], now.getMinutes()) &&
-                            valide(job.conds[1], now.getHours()) &&
-                            (valide(job.conds[2], now.getDay()) ||
-                                    0 === now.getDay() &&
-                                    valide(job.conds[2], 7)) &&
-                            valide(job.conds[3], now.getMonth() + 1) &&
-                            valide(job.conds[4], now.getDate()))
-                        setTimeout(run, 0, job);
-                }
-            }, 60000);
-
-        return cronID;
-    }; // setCron()
+        // Activer éventuellement la tâche.
+        if (status) this.start();
+    }; // Cron()
 
     /**
-     * Supprimer une tâche.
+     * Activer la tâche.
      *
      * @example
-     * var cronID = setCron(alert, "0,30 * * * *", "Ding ! Dong !");
+     * var cron = new Cron("0,30 * * * *", false, alert, "Ding ! Dong !");
      * // ...
-     * clearCron(cronID);
+     * cron.start();
      *
-     * @param {!number} cronID - l'identifiant de la tâche à supprimer.
-     * @see setCron
      * @public
      */
-    window.clearCron = function(cronID) {
-        delete jobs[cronID.toString()];
+    Cron.prototype.start = function () {
+        // Si la tâche est déjà active : ne rien faire.
+        if (this.status()) return;
 
-        if (0 === Object.keys(jobs).length) {
+        // Ajouter la tâche à la liste de tâches actives.
+        jobs.push(this.job);
+
+        // Si c'est la première tâche à être activée : créer la boucle qui
+        // vérifiera chaque minute si des tâches doivent-être exécutées.
+        if (1 === jobs.length)
+            intervalID = setInterval(daemon, 60000);
+    }; // start()
+
+    /**
+     * Désactiver la tâche.
+     *
+     * @example
+     * var cron = new Cron("0,30 * * * *", alert, "Ding ! Dong !");
+     * // ...
+     * cron.stop();
+     *
+     * @public
+     */
+    Cron.prototype.stop = function () {
+        // Si la tâche est déjà inactive : ne rien faire.
+        if (!this.status()) return;
+
+        // Enlever la tâche de la liste des tâches actives.
+        jobs.splice(jobs.indexOf(this.job), 1);
+
+        // S'il n'y a plus de tâches actives : arrêter la boucle.
+        if (!jobs.length)
             clearInterval(intervalID);
-            intervalID = null;
-        }
-    }; // clearCron()
+    }; // stop()
 
-})();
+    /**
+     * Récupérer l'état de la tâche (active ou non).
+     *
+     * @example
+     * var cron = new Cron("0,30 * * * *", alert, "Ding ! Dong !");
+     * console.log(cron.status()); // true.
+     * // ...
+     * cron.stop();
+     * console.log(cron.status()); // false.
+     *
+     * @return {!boolean} Retourne <code>true</code> si la tâche est active ;
+     *                    sinon <code>false</code>.
+     * @public
+     */
+    Cron.prototype.status = function () {
+        return -1 !== jobs.indexOf(this.job);
+    }; // status()
+
+    // Exposer la classe Cron.
+    return Cron;
+
+}));
